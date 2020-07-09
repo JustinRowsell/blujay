57f8c478897f3c6893d30282e0b68feab51c987a
JustinRowsell <Rowsell64@gmail.com>
2020-07-08 19:56:08 -0600

    columns working on landing page

--- blujay/src/app/components/track-list/track-list.component.ts
+++ blujay/src/app/components/track-list/track-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TrackService } from 'src/app/services/track/track.service';
 import { Observable } from 'rxjs';
 import { Track } from 'src/app/models/track';
+import { map, tap } from 'rxjs/operators';
+import { reverse } from 'dns';
 
 @Component({
   selector: 'app-track-list',
@@ -10,12 +12,34 @@ import { Track } from 'src/app/models/track';
 })
 export class TrackListComponent implements OnInit {
 
-  tracks: Observable<Track[]>;
+  tracksArr: Observable<Array<Track[]>>;
+  columnCount = 4;
+
   constructor(private _trackService: TrackService) { }
 
   ngOnInit(): void {
     this._trackService.loadTracks();
-    this.tracks = this._trackService.tracks;
-  }
+    this.tracksArr = this._trackService.tracks.pipe(
+      map(tracks => {
+        const len = Math.floor((tracks.length + 1) / this.columnCount);
+        if (len === 0) {
+          return [[]];
+        }
+        const rem = tracks.length % this.columnCount;
+        console.log(len);
+        const chunks = [];
+        let i = 0;
+        const n = tracks.length;
+        while (i < (n - rem)) {
+          chunks.push(tracks.slice(i, i += len));
+        }
 
+        for (let j = 0; j < rem; j++) {
+          chunks[j].push(tracks[tracks.length - (j + 1)]);
+        }
+        return chunks;
+      }),
+      tap(what => console.log(what))
+    );
+  }
 }
