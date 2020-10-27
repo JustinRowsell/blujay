8c4ef0e8c31188b509a8d56a74fdc322d745b254
Justin Rowsell <Rowsell64@gmail.com>
2020-10-26 21:56:48 -0600

    pointing dev db to a public s3 track

--- blujay_api/priv/repo/seeds.exs
+++ blujay_api/priv/repo/seeds.exs
@@ -15,35 +15,35 @@ alias BlujayApi.Tracks.Track
 
 Track |> Repo.delete_all
 
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/titties_huge.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
       description: "A fire ass beat", title: "J. Cole Type Beat", image: "/assets/images/j_cole.jpg",
       price: 4.99}
       |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/drugs_x.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "Another fire ass beat", title: "Pop Smoke Type Beat",
        image: "/assets/images/popsmoke.jpg", price: 10.0}
        |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/bitch_ass_in_kitchen_z.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "That new new", title: "The butterfly effect", image: "/assets/images/bonfire1.jpg", price: 999.99}
        |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/good_night.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "Anova one bruv.", title: "Fish and Chips", image: "/assets/images/bonfire2.jpg", price: 0.0}
        |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/good_night.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "Anova one bruv.", title: "Fish and Chips", image: "/assets/images/bonfire2.jpg", price: 0.0}
        |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/good_night.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "Anova one bruv.", title: "Fish and Chips", image: "/assets/images/bonfire2.jpg", price: 0.0}
        |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/good_night.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "Anova one bruv.", title: "Fish and Chips", image: "/assets/images/bonfire2.jpg", price: 0.0}
        |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/good_night.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "Anova one bruv.", title: "Fish and Chips", image: "/assets/images/bonfire2.jpg", price: 0.0}
        |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/good_night.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "Anova one bruv.", title: "Fish and Chips", image: "/assets/images/bonfire2.jpg", price: 0.0}
        |> Repo.insert!
-%Track{file: "https://www.wavsource.com/snds_2020-06-10_7014036401687385/tv/south_park/good_night.wav",
+%Track{file: "https://blujay-free.s3.us-east-2.amazonaws.com/Mac_Miller-Salamander.mp3",
        description: "Anova one bruv.", title: "Fish and Chips", image: "/assets/images/bonfire2.jpg", price: 0.0}
        |> Repo.insert!
