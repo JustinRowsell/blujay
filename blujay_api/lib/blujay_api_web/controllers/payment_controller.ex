b800f639df1d5f6fbc364bc9fb972e5ed15f6247
JustinRowsell <Rowsell64@gmail.com>
2020-12-15 22:17:42 -0700

    remove payment from phx api

--- blujay_api/lib/blujay_api_web/controllers/payment_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule BuljayWebApi.PaymentController do
-  alias Stripe
-  def gen_intent() do
-    {:ok, setup_intent} = Stripe.SetupIntent.create(%{})
-
-    # Return the ID to your frontend, and pass it to the confirmCardSetup method from Stripe elements
-    {:ok, setup_intent.id}
-  end
-end
